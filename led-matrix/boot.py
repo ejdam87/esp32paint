1e46e31a2e7c8da51c5a4f010e0b4dd50b2e7922
ejdam87 <adam.dzadon@gmail.com>
2023-01-22 21:28:50 +0100

    Driver + test program add

---
+++ led-matrix/boot.py
@@ -0,0 +1,8 @@
+from machine import Pin, SPI
+from driver import Max7219
+
+spi = SPI(1, baudrate=10000000)
+screen = Max7219(32, 8, spi, Pin(15))
+# screen.rect(0, 0, 32, 8, 1)  # Draws a frame
+# screen.text('Hi!', 4, 4, 1)
+screen.show()
