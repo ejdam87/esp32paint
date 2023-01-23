8e2bd7ce440deeebe4989725e8e80b367419660b
ejdam87 <adam.dzadon@gmail.com>
2023-01-23 23:24:46 +0100

    Upgrade driver

--- led-matrix/boot.py
+++ led-matrix/boot.py
@@ -4,5 +4,5 @@ from driver import Max7219
 spi = SPI(1, baudrate=10000000)
 screen = Max7219(32, 8, spi, Pin(15))
 # screen.rect(0, 0, 32, 8, 1)  # Draws a frame
-# screen.text('Hi!', 4, 4, 1)
+screen.text('', 1, 1, 1)
 screen.show()
